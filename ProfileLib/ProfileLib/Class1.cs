d63720b8b3ea6787c8fe19ec1d40316d573b9bab
Reva <111722589+rssathe08311@users.noreply.github.com>
2023-12-17 17:41:02 -0500

    Profile Library
    - Reva Sathe

---
+++ ProfileLib/ProfileLib/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileLib
+{
+    public class Profile
+    {
+        public string username
+        {
+            get; set;
+        }
+        public string password
+        {
+            get; set;
+        }
+        public string email
+        {
+            get;set;
+        }
+
+        public string status
+        {
+            get; set;
+        }
+
+        public Boolean privacy
+        {
+            get;set;
+        }
+
+        //need to impliment more
+        public string profilePicture;
+
+        public Profile()
+        {
+            this.privacy = false;
+            this.status = "Hi I'm a new user on RITeams!";
+        }
+    }
+}
